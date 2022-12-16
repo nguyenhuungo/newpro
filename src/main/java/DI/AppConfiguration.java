fa8b1d71122bf2f4c422be3b96a1123c42d7ee01
NgoNH <ngonh@ominext.com>
2022-12-16 17:45:21 +0700

    update code

--- src/main/java/DI/AppConfiguration.java
+++ src/main/java/DI/AppConfiguration.java
@@ -3,7 +3,7 @@ package DI;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@ComponentScan("DI")
+@ComponentScan("DI")
 @Configuration
 public class AppConfiguration  {
 }
